003699117ca1c19d3d83366463257f592ec1ba6c
Jagdish918 <127976519+Jagdish918@users.noreply.github.com>
2023-04-03 14:47:08 +0530

    Update letters.py

--- letters.py
+++ letters.py
@@ -1,5 +1,5 @@
 def most_frequent(string):
-   
+    # create a dictionary to store the frequency of each letter
     freq = {}
     for letter in string:
         if letter in freq:
@@ -7,9 +7,12 @@ def most_frequent(string):
         else:
             freq[letter] = 1
 
-  
+    # sort the dictionary by frequency, in descending order
     sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 
- 
+    # print the letters and their frequencies in descending order
     for letter, count in sorted_freq:
         print(f"{letter} = {count:02}")
+
+s=input("Enter a String :")
+most_frequent(s)
