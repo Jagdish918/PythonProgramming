d7f0127a01d65711402d12fac256b74838cc4b2d
Jagdish918 <127976519+Jagdish918@users.noreply.github.com>
2023-04-03 13:53:21 +0530

    letters.py

---
+++ letters.py
@@ -0,0 +1,15 @@
+def most_frequent(string):
+   
+    freq = {}
+    for letter in string:
+        if letter in freq:
+            freq[letter] += 1
+        else:
+            freq[letter] = 1
+
+  
+    sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+ 
+    for letter, count in sorted_freq:
+        print(f"{letter} = {count:02}")
